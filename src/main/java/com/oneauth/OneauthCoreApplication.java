4597e4e5aaf460efbce4e7ba3e973b6605823c2f
jeevanprotco <jeevan.s@protco.in>
2025-12-25 22:02:58 +0530

    Initial Spring Boot setup for OneAuth

---
+++ src/main/java/com/oneauth/OneauthCoreApplication.java
@@ -0,0 +1,13 @@
+package com.oneauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneauthCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneauthCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oneauth/OneauthCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oneauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneauthCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
